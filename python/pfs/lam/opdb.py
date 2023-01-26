333712cc339b4b83402418ae3058fac99ef56783
Fabrice Madec <fabrice.madec@lam.fr>
2023-01-26 08:37:14 +0000

    visit_set_id was replaced by iic_sequence_id in opdb database

--- python/pfs/lam/opdb.py
+++ python/pfs/lam/opdb.py
@@ -15,18 +15,18 @@ import re
 #    return visit_min, visit_max
 
 def fetch_sps_sequence(visit_set_id):
-    sql_all = f"select iic_sequence.visit_set_id, sequence_type, name, comments,sps_exposure.pfs_visit_id, cmd_str, exp_type, sps_module_id, arm, notes, data_flag,time_exp_start,status from sps_exposure \
+    sql_all = f"select iic_sequence.iic_sequence_id, sequence_type, name, comments,sps_exposure.pfs_visit_id, cmd_str, exp_type, sps_module_id, arm, notes, data_flag,time_exp_start,status from sps_exposure \
     inner join visit_set on sps_exposure.pfs_visit_id=visit_set.pfs_visit_id \
-    inner join iic_sequence on visit_set.visit_set_id=iic_sequence.visit_set_id \
+    inner join iic_sequence on visit_set.iic_sequence_id=iic_sequence.iic_sequence_id \
     inner join sps_visit on sps_exposure.pfs_visit_id=sps_visit.pfs_visit_id \
     inner join sps_camera on sps_exposure.sps_camera_id = sps_camera.sps_camera_id \
     left outer join sps_annotation on sps_exposure.pfs_visit_id=sps_annotation.pfs_visit_id \
-    WHERE iic_sequence.visit_set_id = {visit_set_id}"
+    WHERE iic_sequence.iic_sequence_id = {visit_set_id}"
     df = utils.fetch_query(opdb.OpDB.url, sql_all)
     return df
 
 def getVisitRange(visit_set_id):
-    query = f"select min(pfs_visit_id) as visitStart, max(pfs_visit_id) as visitEnd from visit_set WHERE visit_set_id  = {visit_set_id}"
+    query = f"select min(pfs_visit_id) as visitStart, max(pfs_visit_id) as visitEnd from visit_set WHERE iic_sequence_id  = {visit_set_id}"
 
     df = utils.fetch_query(opdb.OpDB.url, query)
     visit_min = df.visitstart.values[0]
@@ -34,7 +34,7 @@ def getVisitRange(visit_set_id):
     return int(visit_min), int(visit_max)
 
 def get_Visit_Set_Id(visit):
-    query = f'select visit_set_id from visit_set WHERE pfs_visit_id = {visit}'
+    query = f'select iic_sequence_id from visit_set WHERE pfs_visit_id = {visit}'
     df = utils.fetch_query(opdb.OpDB.url, query)
     return df.values[0][0]
 
