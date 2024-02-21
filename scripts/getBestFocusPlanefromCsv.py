1a696ac8007bf1f6bac34f6fde3887773c3495a8
Fabrice Madec <fabrice.madec@lam.fr>
2024-02-20 23:08:39 -1000

    reorganise script and lib for the bestFocus and imqual scripts

---
+++ scripts/getBestFocusPlanefromCsv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""
+Executable script to calculate SpS camera best focus from csv files that give peak information.
+"""
+
+from pfs.lam.bestFocusPlanefromCsv import main
+main()
---
+++ scripts/getImqual2Csv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+"""
+Executable script to calculate SpS camera image quality and store them in csv files.
+"""
+
+from pfs.lam.imqual2Csv import main
+main()
